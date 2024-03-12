16b2d7f6fcc7a52b93df0be8d0d972560b65dcf3
EloyRocha97 <rochaeloy11@gmail.com>
2024-03-12 12:21:44 -0300

    Sacamos la Carga hasta proximo aviso xd

--- client/src/views/Contacto/contacto.jsx
+++ client/src/views/Contacto/contacto.jsx
@@ -22,9 +22,9 @@ const Contacto = () => {
           <iframe
             className={style.map}
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1702.8755230165964!2d-64.27033804357053!3d-31.393427364725447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94329f1ee5ef67b7%3A0xd59e5978f38ad3!2sBatall%C3%B3n%2011%20Gral.%20Jos%C3%A9%20Mar%C3%ADa%20Paz.!5e0!3m2!1ses-419!2sar!4v1708197371125!5m2!1ses-419!2sar"
-            allowfullscreen=""
+            allowFullScreen=""
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
       </div>
