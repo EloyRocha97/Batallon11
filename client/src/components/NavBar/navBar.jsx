df868ab89c2d2844a1f190c3828dac43a3bbeaec
Julian Llop <juliianllop@gmail.com>
2024-03-14 17:54:07 -0300

    commit del yulian

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./navBar.module.css";
+import Bata from "../../views/Landing/LandingFotos/bata11.png";
+import LogoExp from "../../views/Landing/LandingFotos/LogoExp.png";
 
 const NavBar = () => {
-  const handleClick = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+    const handleClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
 
-  return (
-    <div className={style.navBar}>
-      <Link to="/" className={style.link} onClick={handleClick}>
-        Inicio
-      </Link>
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSconCnaQC6sToSQrx7B1rWyb2GCpP3OuGnzLS5FUN48nhow4Q/viewform"
-        className={style.link}
-        onClick={handleClick}
-      >
-        Inscripciones
-      </a>
-      <Link to="/banda" className={style.link} onClick={handleClick}>
-        Banda
-      </Link>
-      {/* <Link className={style.link} onClick={handleClick}>
-        Ficha Medica
-      </Link> */}
-    </div>
-  );
+    return (
+        <div className={style.navBar}>
+            <div className={style.links}>
+                <Link to="/" className={style.link} onClick={handleClick}>
+                    Inicio
+                </Link>
+                <a
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSconCnaQC6sToSQrx7B1rWyb2GCpP3OuGnzLS5FUN48nhow4Q/viewform"
+                    className={style.link}
+                    onClick={handleClick}
+                >
+                    Inscripciones
+                </a>
+                <Link to="/banda" className={style.link} onClick={handleClick}>
+                    Banda
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default NavBar;
--- client/src/views/Landing/landing.jsx
+++ client/src/views/Landing/landing.jsx
@@ -10,102 +10,134 @@ import style from "./landing.module.css";
 import Contacto from "../Contacto/contacto";
 
 const Inicio = () => {
-  return (
-    <div>
-      <div className={style.presentacion}>
-        <div className={style.logoWrapper}>
-          <img src={Bata} className={style.imageBata} alt="bata11" />
-          <div className={style.overlay}>
-            <div className={style.logoContainer}>
-              <img src={Logo} className={style.imageLogo} alt="Logo" />
+    return (
+        <div className={style.landing}>
+            <NavBar />
+            <div className={style.homeText}>
+                <div>
+                    <h3 className={style.titles}>Quienes somos?</h3>
+                    <p className={style.text}>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Sunt veritatis error nisi quod quisquam. Sed
+                        commodi impedit adipisci dolorem voluptatibus,
+                        consectetur architecto maxime eaque quibusdam odit.
+                        Autem eaque tempora repellat! Lorem, ipsum dolor sit
+                        amet consectetur adipisicing elit. Unde officiis vel
+                        dignissimos beatae esse! Pariatur odio dolores eveniet
+                        atque enim suscipit repellat? Fugit cumque dignissimos
+                        illo odit officiis amet non! Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Id alias cupiditate neque
+                        maiores cumque ex aliquam fuga. Numquam sed dolorem
+                        necessitatibus consequatur eos. Delectus temporibus illo
+                        eos perspiciatis! Alias, repellendus? Lorem ipsum dolor
+                        sit amet consectetur adipisicing elit. Sequi aperiam
+                        consectetur impedit perferendis, natus dolor cum nam.
+                        Saepe, laboriosam illum numquam aperiam dolore fuga,
+                        quisquam ullam unde esse nemo accusamus!
+                    </p>
+                </div>
+                <div>
+                    <h3 className={style.titles}>Que hacemos?</h3>
+                    <p className={style.text}>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Sunt veritatis error nisi quod quisquam. Sed
+                        commodi impedit adipisci dolorem voluptatibus,
+                        consectetur architecto maxime eaque quibusdam odit.
+                        Autem eaque tempora repellat! Lorem, ipsum dolor sit
+                        amet consectetur adipisicing elit. Unde officiis vel
+                        dignissimos beatae esse! Pariatur odio dolores eveniet
+                        atque enim suscipit repellat? Fugit cumque dignissimos
+                        illo odit officiis amet non! Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Id alias cupiditate neque
+                        maiores cumque ex aliquam fuga. Numquam sed dolorem
+                        necessitatibus consequatur eos. Delectus temporibus illo
+                        eos perspiciatis! Alias, repellendus? Lorem ipsum dolor
+                        sit amet consectetur adipisicing elit. Sequi aperiam
+                        consectetur impedit perferendis, natus dolor cum nam.
+                        Saepe, laboriosam illum numquam aperiam dolore fuga,
+                        quisquam ullam unde esse nemo accusamus!
+                    </p>
+                </div>
+                <div>
+                    <h3 className={style.titles}>Nuestra banda de musica</h3>
+                    <p className={style.text}>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Sunt veritatis error nisi quod quisquam. Sed
+                        commodi impedit adipisci dolorem voluptatibus,
+                        consectetur architecto maxime eaque quibusdam odit.
+                        Autem eaque tempora repellat! Lorem, ipsum dolor sit
+                        amet consectetur adipisicing elit. Unde officiis vel
+                        dignissimos beatae esse! Pariatur odio dolores eveniet
+                        atque enim suscipit repellat? Fugit cumque dignissimos
+                        illo odit officiis amet non! Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Id alias cupiditate neque
+                        maiores cumque ex aliquam fuga. Numquam sed dolorem
+                        necessitatibus consequatur eos. Delectus temporibus illo
+                        eos perspiciatis! Alias, repellendus? Lorem ipsum dolor
+                        sit amet consectetur adipisicing elit. Sequi aperiam
+                        consectetur impedit perferendis, natus dolor cum nam.
+                        Saepe, laboriosam illum numquam aperiam dolore fuga,
+                        quisquam ullam unde esse nemo accusamus!
+                    </p>
+                </div>
             </div>
-            <div className={style.logoContainerDb}>
-              <img src={db} className={style.imageLogoDb} alt="Logo" />
+            <div className={style.final}>
+                <div className={style.buttons}>
+                    <a
+                        href="https://forms.gle/4uE4jaSe5oTspBZg7"
+                        className={style.link}
+                    >
+                        Pre-Inscripciones
+                    </a>
+                    <a href="/salidas" className={style.link}>
+                        Nuestras Fotos
+                    </a>
+                </div>
+                <Contacto />
             </div>
-            <div className={style.logoContainer}>
-              <img src={LogoExp} className={style.imageLogo2} alt="Logo" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <NavBar />
-      <div className={style.homeText}>
-        <h3 className={style.items}>Quienes somos?</h3>
-        <p className={style.parrafo11}>
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoText
-          oTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTexto
-        </p>
-        <h3 className={style.items}>Que hacemos?</h3>
-        <p className={style.parrafo11}>
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoText
-          oTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTexto
-        </p>
-        <h3 className={style.items}>Nuestra banda de musica</h3>
-        <p className={style.parrafo111}>
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoText
-          oTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto
-          TextoTextoTexto
-        </p>
-      </div>
-      <div className={style.final}>
-        <div className={style.buttons}>
-          <a href="https://forms.gle/4uE4jaSe5oTspBZg7" className={style.link}>
-            Pre-Inscripciones
-          </a>
-          <a href="/salidas" className={style.link}>
-            Nuestras Fotos
-          </a>
-        </div>
-        <Contacto />
-      </div>
-      <div className={style.redes}>
-        <div>
-          <p>No te olvides de visitarnos por nuestras redes</p>
-        </div>
-        <div className={style.redesIcon}>
-          <a
-            href="https://www.instagram.com/batallon11/?hl=es"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={Ig} alt="Mi ig" className={style.redesIconTam} />
-          </a>
+            <div className={style.redes}>
+                <div>
+                    <p>No te olvides de visitarnos por nuestras redes</p>
+                </div>
+                <div className={style.redesIcon}>
+                    <a
+                        href="https://www.instagram.com/batallon11/?hl=es"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            src={Ig}
+                            alt="Mi ig"
+                            className={style.redesIconTam}
+                        />
+                    </a>
 
-          <a
-            href="https://www.facebook.com/Batallon11Cordoba/?locale=es_LA"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={Face} alt="Mi face" className={style.redesIconTam} />
-          </a>
-          <a
-            href="https://www.youtube.com/user/Batallon11Cba"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={Yt} alt="Mi Yt" className={style.redesIconTam} />
-          </a>
+                    <a
+                        href="https://www.facebook.com/Batallon11Cordoba/?locale=es_LA"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            src={Face}
+                            alt="Mi face"
+                            className={style.redesIconTam}
+                        />
+                    </a>
+                    <a
+                        href="https://www.youtube.com/user/Batallon11Cba"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            src={Yt}
+                            alt="Mi Yt"
+                            className={style.redesIconTam}
+                        />
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Inicio;
