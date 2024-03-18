8cb34bd13734fdc188b9f2ff1358a4408d234b33
Julian Llop <juliianllop@gmail.com>
2024-03-18 16:41:02 -0300

    select height

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import style from "./navBar.module.css";
+import { FormControl, TextField, MenuItem } from "@mui/material";
 
 const NavBar = () => {
     const [selectedOption, setSelectedOption] = useState("");
