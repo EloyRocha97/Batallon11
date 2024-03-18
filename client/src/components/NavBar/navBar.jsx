c273031dfec80af19a2f43f9e6f6f4f169f88cef
Julian Llop <juliianllop@gmail.com>
2024-03-18 15:01:58 -0300

    navBar select

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -3,58 +3,62 @@ import { Link } from "react-router-dom";
 import style from "./navBar.module.css";
 
 const NavBar = () => {
-  const [selectedOption, setSelectedOption] = useState("");
+    const [selectedOption, setSelectedOption] = useState("");
 
-  const handleClick = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+    const handleClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
 
-  const handleOptionChange = (event) => {
-    const selected = event.target.value;
-    setSelectedOption(selected);
-    if (selected === "batallon") {
-      window.location.href = "/batallon";
-    } else if (selected === "banda") {
-      window.location.href = "/banda";
-    }
-  };
+    const handleOptionChange = (event) => {
+        const selected = event.target.value;
+        setSelectedOption(selected);
+        if (selected === "batallon") {
+            window.location.href = "/batallon";
+        } else if (selected === "banda") {
+            window.location.href = "/banda";
+        }
+    };
 
-  return (
-    <div className={style.navBar}>
-      <div className={style.container}>
-        <div className={style.batallon}>
-          <h1>Batallón 11</h1>
-          <h1>Gral. José María Paz</h1>
+    return (
+        <div className={style.navBar}>
+            <div className={style.container}>
+                <div className={style.batallon}>
+                    <h1>Batallón 11</h1>
+                    <h1>Gral. José María Paz</h1>
+                </div>
+            </div>
+            <div className={style.links}>
+                <Link to="/" className={style.link} onClick={handleClick}>
+                    Inicio
+                </Link>
+                <a
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSconCnaQC6sToSQrx7B1rWyb2GCpP3OuGnzLS5FUN48nhow4Q/viewform"
+                    className={style.link}
+                    onClick={handleClick}
+                >
+                    Inscripciones
+                </a>
+                <select
+                    value={selectedOption}
+                    onChange={handleOptionChange}
+                    className={style.select}
+                >
+                    <option value="" disabled hidden className={style.option}>
+                        Historia
+                    </option>
+                    <option value="batallon" className={style.option}>
+                        Batallón
+                    </option>
+                    <option value="banda" className={style.option}>
+                        Banda
+                    </option>
+                </select>
+            </div>
         </div>
-      </div>
-      <div className={style.links}>
-        <Link to="/" className={style.link} onClick={handleClick}>
-          Inicio
-        </Link>
-        <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSconCnaQC6sToSQrx7B1rWyb2GCpP3OuGnzLS5FUN48nhow4Q/viewform"
-          className={style.link}
-          onClick={handleClick}
-        >
-          Inscripciones
-        </a>
-        <select
-          value={selectedOption}
-          onChange={handleOptionChange}
-          className={style.select}
-        >
-          <option value="" disabled hidden>
-            Historia
-          </option>
-          <option value="batallon">Batallón</option>
-          <option value="banda">Banda</option>
-        </select>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default NavBar;
