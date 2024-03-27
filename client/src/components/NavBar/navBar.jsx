d83940511096d82216ff23ddb5074968cdcfed63
Julian Llop <juliianllop@gmail.com>
2024-03-27 19:34:34 -0300

    navBar responsive

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -1,99 +1,320 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import style from "./navBar.module.css";
-import { FormControl, TextField, MenuItem } from "@mui/material";
+import {
+    FormControl,
+    TextField,
+    MenuItem,
+    Box,
+    Button,
+    Typography,
+} from "@mui/material";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import logo11 from "../../views/Landing/LandingFotos/bata11.png";
 
 const NavBar = () => {
-  const [selectedOption, setSelectedOption] = useState("");
+    const [selectedOption, setSelectedOption] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+    const handleOpenMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
-  const handleOptionChange = (event) => {
-    const selected = event.target.value;
-    setSelectedOption(selected);
-    if (selected === "batallon") {
-      window.location.href = "/batallon";
-    } else if (selected === "banda") {
-      window.location.href = "/banda";
-    }
-  };
+    const isPhone = useMediaQuery("(max-width:500px)");
 
-  const handleOptionChangeN = (event) => {
-    const selected = event.target.value;
-    setSelectedOption(selected);
-    if (selected === "E.A.D.B.") {
-      window.location.href = "https://eadb.org.ar/";
-    } else if (selected === "M.J.S") {
-      window.location.href = "/mjs";
-    }
-  };
+    const handleClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
 
-  return (
-    <div className={style.navBar}>
-      <div className={style.container}>
-        <div className={style.batallon}>
-          <h1>Batallón 11</h1>
-          <h3>Gral. José María Paz</h3>
-        </div>
-      </div>
-      <div className={style.links}>
-        <Link to="/" className={style.link} onClick={handleClick}>
-          Inicio
-        </Link>
-        <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSconCnaQC6sToSQrx7B1rWyb2GCpP3OuGnzLS5FUN48nhow4Q/viewform"
-          className={style.link}
-          onClick={handleClick}
+    const handleOptionChange = (event) => {
+        const selected = event.target.value;
+        setSelectedOption(selected);
+        if (selected === "batallon") {
+            window.location.href = "/batallon";
+        } else if (selected === "banda") {
+            window.location.href = "/banda";
+        }
+    };
+
+    const handleOptionChangeN = (event) => {
+        const selected = event.target.value;
+        setSelectedOption(selected);
+        if (selected === "E.A.D.B.") {
+            window.location.href = "https://eadb.org.ar/";
+        } else if (selected === "M.J.S") {
+            window.location.href = "/mjs";
+        }
+    };
+
+    return (
+        <Box
+            sx={{
+                minWidth: "100%",
+            }}
         >
-          Inscripciones
-        </a>
-        <Link to="/bandaM" className={style.link} onClick={handleClick}>
-          Banda
-        </Link>
+            {isPhone ? (
+                <Box>
+                    <Box
+                        sx={{
+                            display: isPhone ? "flex" : "none",
+                            minWidth: "100%",
+                            height: "61px",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            backgroundColor: "rgba(0, 20, 78)",
+                            // zIndex: 10,
+                        }}
+                    >
+                        <Button
+                            variant="contained"
+                            onClick={handleOpenMenu}
+                            sx={{
+                                display: isPhone ? "flex" : "none",
+                                minWidth: "60px",
+                                height: "59px",
+                                borderRadius: "0px",
+                                backgroundColor: "rgba(0, 20, 78)",
+                                color: "rgba(0, 20, 78)",
 
-        <select
-          value={selectedOption}
-          onChange={handleOptionChangeN}
-          className={style.select}
-        >
-          <option value="" disabled hidden className={style.option}>
-            Nosotros
-          </option>
-          <option value="E.A.D.B." className={style.option}>
-            E.A.D.B.
-          </option>
-          <option value="M.J.S" className={style.option}>
-            M.J.S
-          </option>
-        </select>
+                                "&:hover": {
+                                    backgroundColor: "rgba(0, 20, 78)",
+                                },
+                            }}
+                        >
+                            {isOpen ? (
+                                <CloseOutlinedIcon
+                                    sx={{
+                                        color: "white",
+                                    }}
+                                />
+                            ) : (
+                                <MenuOutlinedIcon
+                                    sx={{
+                                        color: "white",
+                                    }}
+                                />
+                            )}
+                        </Button>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    color: "white",
+                                    fontSize: "clamp(1px, 4vw, 3.5rem)",
+                                }}
+                            >
+                                Batallón 11
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    color: "white",
+                                    fontSize: "clamp(1px, 3vw, 3rem)",
+                                }}
+                            >
+                                Gral. José María Paz
+                            </Typography>
+                        </Box>
 
-        <Link to="/mistica" className={style.link} onClick={handleClick}>
-          Mistica
-        </Link>
+                        <img
+                            src={logo11}
+                            alt="logo11"
+                            className={style.logo11}
+                        />
+                    </Box>
+                    {isOpen && (
+                        <Box
+                            sx={{
+                                height: "200px",
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "start",
+                                gap: "10px",
+                                backgroundColor: "rgba(0, 20, 78, 0.9)",
+                                padding: "20px",
 
-        <select
-          value={selectedOption}
-          onChange={handleOptionChange}
-          className={style.select}
-        >
-          <option value="" disabled hidden className={style.option}>
-            Historia
-          </option>
-          <option value="batallon" className={style.option}>
-            Batallón
-          </option>
-          <option value="banda" className={style.option}>
-            Banda
-          </option>
-        </select>
-      </div>
-    </div>
-  );
+                            }}
+                        >
+                            <Link
+                                to="/"
+                                className={style.phoneLink}
+                                onClick={handleClick}
+                            >
+                                Inicio
+                            </Link>
+                            <a
+                                href="https://docs.google.com/forms/d/e/1FAIpQLSconCnaQC6sToSQrx7B1rWyb2GCpP3OuGnzLS5FUN48nhow4Q/viewform"
+                                className={style.phoneLink}
+                                onClick={handleClick}
+                            >
+                                Inscripciones
+                            </a>
+                            <Link
+                                to="/bandaM"
+                                className={style.phoneLink}
+                                onClick={handleClick}
+                            >
+                                Banda
+                            </Link>
+
+                            <Link
+                                to="/mistica"
+                                className={style.phoneLink}
+                                onClick={handleClick}
+                            >
+                                Mistica
+                            </Link>
+
+                            <select
+                                value={selectedOption}
+                                onChange={handleOptionChange}
+                                className={style.phoneSelect}
+                            >
+                                <option
+                                    value=""
+                                    disabled
+                                    hidden
+                                    className={style.phoneOption}
+                                >
+                                    Historia
+                                </option>
+                                <option
+                                    value="batallon"
+                                    className={style.phoneOption}
+                                >
+                                    Batallón
+                                </option>
+                                <option
+                                    value="banda"
+                                    className={style.phoneOption}
+                                >
+                                    Banda
+                                </option>
+                            </select>
+                            <select
+                                value={selectedOption}
+                                onChange={handleOptionChangeN}
+                                className={style.phoneSelect}
+                            >
+                                <option
+                                    value=""
+                                    disabled
+                                    hidden
+                                    className={style.phoneOption}
+                                >
+                                    Nosotros
+                                </option>
+                                <option
+                                    value="E.A.D.B."
+                                    className={style.phoneOption}
+                                >
+                                    E.A.D.B.
+                                </option>
+                                <option
+                                    value="M.J.S"
+                                    className={style.phoneOption}
+                                >
+                                    M.J.S
+                                </option>
+                            </select>
+                        </Box>
+                    )}
+                </Box>
+            ) : (
+                <div className={!isPhone ? style.navBar : style.phoneNavBar}>
+                    <div className={style.container}>
+                        <div className={style.batallon}>
+                            <h1>Batallón 11</h1>
+                            <h3>Gral. José María Paz</h3>
+                        </div>
+                    </div>
+                    <div className={style.links}>
+                        <Link
+                            to="/"
+                            className={style.link}
+                            onClick={handleClick}
+                        >
+                            Inicio
+                        </Link>
+                        <a
+                            href="https://docs.google.com/forms/d/e/1FAIpQLSconCnaQC6sToSQrx7B1rWyb2GCpP3OuGnzLS5FUN48nhow4Q/viewform"
+                            className={style.link}
+                            onClick={handleClick}
+                        >
+                            Inscripciones
+                        </a>
+                        <Link
+                            to="/bandaM"
+                            className={style.link}
+                            onClick={handleClick}
+                        >
+                            Banda
+                        </Link>
+
+                        <Link
+                            to="/mistica"
+                            className={style.link}
+                            onClick={handleClick}
+                        >
+                            Mistica
+                        </Link>
+
+                        <select
+                            value={selectedOption}
+                            onChange={handleOptionChange}
+                            className={style.select}
+                        >
+                            <option
+                                value=""
+                                disabled
+                                hidden
+                                className={style.option}
+                            >
+                                Historia
+                            </option>
+                            <option value="batallon" className={style.option}>
+                                Batallón
+                            </option>
+                            <option value="banda" className={style.option}>
+                                Banda
+                            </option>
+                        </select>
+                        <select
+                            value={selectedOption}
+                            onChange={handleOptionChangeN}
+                            className={style.select}
+                        >
+                            <option
+                                value=""
+                                disabled
+                                hidden
+                                className={style.option}
+                            >
+                                Nosotros
+                            </option>
+                            <option value="E.A.D.B." className={style.option}>
+                                E.A.D.B.
+                            </option>
+                            <option value="M.J.S" className={style.option}>
+                                M.J.S
+                            </option>
+                        </select>
+                    </div>
+                </div>
+            )}
+        </Box>
+    );
 };
 
 export default NavBar;
